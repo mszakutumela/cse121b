f76cdec62d2abd276abd93b9795ba155b4e26e52
mszakutumela <misszandlovu@gmail.com>
2023-10-25 04:11:27 +0200

    project in progress

---
+++ .history/Project/scripts/project_20231024205434.js
@@ -0,0 +1,17 @@
+/* Project */
+
+// Short Quiz Section
+
+function add(Number1, Number2, Number3) {
+    return Number1 + Number2 + Number3;
+}
+
+function addTemples() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    let addNumber3 = Number(document.querySelector('#add3').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2, addNumber3);
+}
+document.querySelector('#addTemples').addEventListener('click', addTemples);
+
+function answer() {}
---
+++ .history/Project/scripts/project_20231025032411.js
@@ -0,0 +1,33 @@
+/* Project */
+
+// Short Quiz Section
+
+function add(Number1, Number2, Number3) {
+    return Number1 + Number2 + Number3;
+}
+
+function addTemples() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    let addNumber3 = Number(document.querySelector('#add3').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2, addNumber3);
+}
+document.querySelector('#addTemples').addEventListener('click', addTemples);
+
+const answer = () => {
+    let correct_answer = parseInt(document.querySelector('#answer').value);
+    if (answer == correct_answer) {
+        let result = print("Correct! Well done!");
+        document.querySelector('#answer').value = result;
+    }  
+}  
+  
+    
+    
+    // print("Correct! Well done!");
+    
+    // let wrong_answer = print("Incorrect! Try again!");
+    // if (answer == correct_answer);
+
+    // if (answer == wrong_answer);
+
